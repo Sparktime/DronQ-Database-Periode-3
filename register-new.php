20544605106b4bcdc13433b5e8ab2ad9d6b3d899
Wouter Plaatsman <45604347+Sparktime@users.noreply.github.com>
2019-03-26 20:33:25 +0100

    With SESSION kooeekie

--- register-new.php
+++ register-new.php
@@ -1,6 +1,7 @@
 <?php
 // UTF-8 NΟ BOM
 session_start();
+$_SESSION['list'] = 'database.php';
 require 'db.php';
 
 $email = $_POST['email'];
@@ -12,5 +13,16 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute([$email, $passwordhash]);
 checkSQL($stmt);
 
-header('location: login.php');
+$sql = "INSERT INTO `CUSTOMER` (Email) VALUES(?)";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$email]);
+checkSQL($stmt);
+
+$sql = "SELECT `Customer_ID` FROM `CUSTOMER` INNER JOIN `USER` ON `USER`.`Email` = `CUSTOMER`.`Email` WHERE `USER`.`Email` = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$email]);
+checkSQL($stmt);
+$customerid = $stmt->fetch()[0];
+$_SESSION['email'] = $email;
+header('location: customer-edit.php?Customer_ID='.$customerid);
 
