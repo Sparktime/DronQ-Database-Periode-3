54f74559c0381d4fa19f0a20dddec1caea784c0b
Jos de Poorter <45604472+jampoort@users.noreply.github.com>
2019-06-05 19:35:24 +0200

    product manager

--- productManager.php
+++ productManager.php
@@ -1,37 +1,32 @@
 <?php
-//
-
 
 session_start();
 require 'db.php';
 require 'productController.php';
 
+
+
+        // NEW PRODUCT //
+if($_GET['action'] == 'create') {
+
+    $create = new productController($pdo);
+    $create->create();
+}
+
+    // DELETE PRODUCT //
+if($_GET['action'] == 'delete') {
+
+    $Serial_No = $_GET['Serial_No'];
+
+    $delete = new productController($pdo);
+    $delete->delete($_GET['Serial_No']);
+}
+
+    //  SAVE PRODUCT EDIT //
 if($_GET['action'] == 'save'){
 
     $save = new productController($pdo);
     $save->save($_POST['Serial_No'], $_POST);
 }
 
-if($_GET['action'] == 'create') {
-
-
-    // NEW PRODUCT //
-
-    $create = new productController($pdo);
-    $create->create();
-
-
-
-}
-
-//
-//    // DELETE FUNCTIE //
-//
-//    $Serial_No = $_GET['Serial_No'];
-//
-//    $delete = new productController($pdo);
-//    $delete->delete($_GET['Serial_No']);
-//
-//
-//
 header('location: product-list.php');
