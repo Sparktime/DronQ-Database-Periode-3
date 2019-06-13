1f5ea4817df52e8f61eb222afe633e7c3b8e102b
johanpotters <japotter@avans.nl>
2019-06-13 21:42:38 +0200

    Search en Langue

--- webstoreController.php
+++ webstoreController.php
@@ -27,4 +27,11 @@ class webstoreController
         return $this->pdo->query($sql, PDO::FETCH_OBJ);
     }
 
-}
+    public function search($search){
+        
+        $sql = "SELECT * FROM `PRODUCTINFO` WHERE 'Type' LIKE ? OR 'Name' LIKE ? OR 'Text' LIKE ? OR 'Infotext' LIKE ? OR 'Specs' LIKE ?";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([%$search%, %$search%, %$search%, %$search%, %$search%]);
+        checkSQL($stmt);
+        return $stmt->fetchAll(PDO::FETCH_OBJ);
+    }
