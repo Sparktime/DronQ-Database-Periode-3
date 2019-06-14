3c8ccb475ba281a5c1dc33fca903d89a542b47ae
Wouter Plaatsman <45604347+Sparktime@users.noreply.github.com>
2019-06-14 23:46:01 +0200

    menu en create tables.php

--- menu.php
+++ menu.php
@@ -41,12 +41,6 @@
             ?>
             <?php if (isset($_SESSION['level']) == 1) {
                 echo '<li class="nav-item">
-                <a href="productinfo-list.php" class="nav-link">Product Info</a>
-            </li>';
-            }
-            ?>
-            <?php if (isset($_SESSION['level']) == 1) {
-                echo '<li class="nav-item">
                 <a href="order-list.php" class="nav-link">Orders</a>
             </li>';
             }
